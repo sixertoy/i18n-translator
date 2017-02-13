112829fcfce78a45689ec842c75ace677fff6a56
sixertoy <sixertoy.github@gmail.com>
2017-02-13 19:44:47 +0100

    update header

--- src/app/views/ApplicationHeader.jsx
+++ src/app/views/ApplicationHeader.jsx
@@ -1,64 +1,41 @@
 import React from 'react';
-import isempty from 'lodash.isempty';
-import isstring from 'lodash.isstring';
-// punkbeer
+// project
 import logo from './../../assets/json_logo.svg';
-import GithubStarButton from './../../core/views/GithubStarButton';
 
-class ApplicationMainMenu extends React.PureComponent {
-
-  constructor (props) {
-    super(props);
-    this.state = {};
-  }
-
-  /* --------------------------------------------------------
-
-   Privates
-
-  -------------------------------------------------------- */
-
-  /* --------------------------------------------------------
-
-   Renders Sub Components
-
-  -------------------------------------------------------- */
-
-  render () {
-    return (
-      <div className="application-header"
+const ApplicationHeader = () => (
+  <div className="application-header flex-columns"
+    style={{
+      textAlign: 'left'
+    }}>
+    <a href="/">
+      <img alt="JSON Translation Online Editor"
         style={{
-          textAlign: 'center'
-        }}>
-        <h1 style={{
-          margin: '10px 0',
-          lineHeight: '1.25em'
-        }}>
-          <img alt="JSON Translation Online Editor"
-            style={{
-              width: '40px',
-              height: '40px',
-              marginRight: '10px',
-              verticalAlign: 'bottom'
-            }}
-            src={logo} />
-          <span>JSON Translation Online Editor</span>
-        </h1>
-        <h2 style={{
-          color: '#8e8e8e',
-          fontSize: '1.43em'
-        }}>
-          <span>Create/Update JSON languages files in a simple way</span>
-        </h2>
-      </div>
-    );
-  }
+          width: '60px',
+          height: '60px',
+          marginRight: '10px',
+          verticalAlign: 'bottom'
+        }}
+        src={logo} />
+    </a>
+    <div>
+      <h1 style={{
+        margin: '0',
+        lineHeight: '1.25em',
+        fontWeight: 'normal'
+      }}>
+        <span>JSON Translation Online Editor</span>
+      </h1>
+      <h2 style={{
+        margin: '0',
+        color: '#8e8e8e',
+        marginTop: '7px',
+        fontWeight: 'normal',
+        fontSize: '1.285715em'
+      }}>
+        <span>Create/Update JSON languages files in a simple way</span>
+      </h2>
+    </div>
+  </div>
+);
 
-}
-
-ApplicationMainMenu.propTypes = {
-  langs: React.PropTypes.array.isRequired,
-  facade: React.PropTypes.object.isRequired
-};
-
-export default ApplicationMainMenu;
+export default ApplicationHeader;
