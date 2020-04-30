e0493588c63013bc82fedb20549bc3859e19cfd6
sixertoy <sixertoy.github@gmail.com>
2020-04-30 19:41:14 +0200

    remove unused files

--- src/components/observers/index.js
+++
@@ -1,5 +0,0 @@
-import { toMap } from '../../core/utils/ObjectUtils';
-
-const imports = toMap({});
-
-export default imports;
--- src/components/stores/index.js
+++
@@ -1,8 +0,0 @@
-import { toMap } from '../../core/utils/ObjectUtils';
-import ApplicationStore from './ApplicationStore';
-
-const imports = toMap({
-  ApplicationStore,
-});
-
-export default imports;
