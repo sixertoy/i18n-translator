0e953864704cb5f985db4c1750cfaa3f7206cf6a
sixertoy <sixertoy.github@gmail.com>
2020-05-06 18:44:03 +0200

    sync steps with routes path

--- src/components/views/import/index.jsx
+++ src/components/views/import/index.jsx
@@ -2,7 +2,13 @@ import get from 'lodash.get';
 import React, { useEffect, useState } from 'react';
 import { createUseStyles } from 'react-jss';
 // import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import {
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+  useLocation,
+} from 'react-router-dom';
 
 import Steps from '../../commons/Steps';
 import withLayout from '../../layout';
@@ -32,38 +38,56 @@ const CREATE_STEPS = [
   { label: 'Importer', path: '/import/editor' },
 ];
 
+function getPathByStepIndex(index) {
+  const path = get(CREATE_STEPS, `${index}.path`);
+  return path;
+}
+
+function getStepIndexByPath(location) {
+  const { pathname } = location;
+  const index = CREATE_STEPS.findIndex(({ path }) => path === pathname);
+  return index;
+}
+
 const ImportViewComponent = () => {
   const classes = useStyles();
   const history = useHistory();
+  const location = useLocation();
 
   const [step, setStep] = useState(0);
   const [lang, setLang] = useState(undefined);
 
   useEffect(() => {
-    const path = get(CREATE_STEPS, `${step}.path`);
-    history.push(path);
-  }, [history, step]);
+    const index = getStepIndexByPath(location);
+    if (index !== step) setStep(index);
+  }, [history, lang, location, step]);
 
   return (
     <div className={classes.import}>
       <Steps current={step} steps={CREATE_STEPS} />
       <Switch>
+        <Redirect exact from="/import" push={false} to="/import/start" />
         <Route exact path="/import/start">
-          <Intro onClick={() => setStep(step + 1)} />
+          <Intro
+            onClick={() => {
+              const path = getPathByStepIndex(1);
+              history.push(path);
+            }}
+          />
         </Route>
         <Route exact path="/import/select">
           <Select
             lang={lang}
             onChange={value => {
+              const path = getPathByStepIndex(2);
+              history.push(path);
               setLang(value);
-              setStep(step + 1);
             }}
           />
         </Route>
         <Route exact path="/import/editor">
           <Editor value="" onChange={() => {}} />
         </Route>
-        <Redirect exact from="/import" push={false} to="/import/start" />
       </Switch>
     </div>
   );
