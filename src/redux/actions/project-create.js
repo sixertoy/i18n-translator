fe6083b6e592d3faacc823187215ab7cc0eb3b8f
sixertoy <sixertoy.github@gmail.com>
2020-05-08 09:46:18 +0200

    update create project action

---
+++ src/redux/actions/project-create.js
@@ -0,0 +1,19 @@
+import { getName } from 'ikea-name-generator';
+import { v1 as uuidv1 } from 'uuid';
+
+import { EVENT_TYPES } from '../../constants';
+
+const createProject = () => {
+  const id = uuidv1();
+  const name = getName();
+  const ctime = Date.now();
+  return {
+    ctime,
+    id,
+    mtime: ctime,
+    name,
+    type: EVENT_TYPES.PROJECT_CREATE,
+  };
+};
+
+export default createProject;
