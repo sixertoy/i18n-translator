e0e4724de964f6e16039529615b3a0495e6a6c06
sixertoy <sixertoy.github@gmail.com>
2020-05-08 18:10:32 +0200

    fix project infos percentage

--- src/components/views/board/options/infos.jsx
+++ src/components/views/board/options/infos.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { createUseStyles, useTheme } from 'react-jss';
 import { useSelector } from 'react-redux';
 
-import { USE_PROJECTS } from '../../../../features.json';
 import { selectPercentages } from '../../../../redux/selectors';
 import PercentageBar from '../../../commons/percentage-bar';
 
 const useStyles = createUseStyles({
   infos: {
-    composes: ['no-flex'],
+    composes: ['flex-1'],
   },
   percentage: {
     composes: ['mt7'],
@@ -28,11 +27,9 @@ const InfosComponent = () => {
   const { project } = useSelector(selectPercentages);
   return (
     <div className={classes.infos}>
-      {USE_PROJECTS && (
-        <h3 className={classes.title}>
-          <span>Nom du projet</span>
-        </h3>
-      )}
+      <h3 className={classes.title}>
+        <span>Nom du projet</span>
+      </h3>
       <PercentageBar
         showPercent
         className={classes.percentage}
