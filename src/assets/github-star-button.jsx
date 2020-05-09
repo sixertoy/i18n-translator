b84b3f0d66c93eac3155138f196d0cdc7574635d
sixertoy <sixertoy.github@gmail.com>
2020-05-09 08:52:57 +0200

    move github and twitter buttons to assets

---
+++ src/assets/github-star-button.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import { name, repository } from '../../package.json';
+
+/**
+ * @see https://buttons.github.io/
+ *
+ * <!-- Place this tag in your head or just before your close body tag. -->
+ * <script async defer src="https://buttons.github.io/buttons.js"></script>
+ */
+const GithubStarButton = () => (
+  <a
+    aria-label={`Star sixertoy/${name} on GitHub"`}
+    className="github-button"
+    data-count-api={`/repos/sixertoy/${name}#stargazers_count`}
+    data-count-aria-label="# stargazers on GitHub"
+    data-count-href={`/sixertoy/${name}/stargazers`}
+    data-style="mega"
+    href={repository.url}>
+    Star
+    <Helmet
+      script={[
+        {
+          src: 'https://buttons.github.io/buttons.js',
+          type: 'text/javascript',
+        },
+      ]}
+    />
+  </a>
+);
+
+export default GithubStarButton;
