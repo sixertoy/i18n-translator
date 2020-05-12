f91b4715b6a25a8670f0619cd2dd05b2bc5f9a8e
sixertoy <sixertoy.github@gmail.com>
2020-05-12 13:22:08 +0200

    add try/catch if firebase configuration is missin

--- src/core/utils/firebase.js
+++ src/core/utils/firebase.js
@@ -4,6 +4,9 @@ const FIREBASE_KEY_PREFIX = 'FIREBASE_';
 export const getFirebaseConfig = () => {
   const keys = Object.entries(ENV);
   const filtered = keys.filter(a => a[0].indexOf(FIREBASE_KEY_PREFIX) !== -1);
+  if (filtered.length) {
+    throw new Error('Unable to load firebase configuration');
+  }
   const reword = filtered.map(([key, value]) => {
     const len = FIREBASE_KEY_PREFIX.length;
     const index = key.indexOf(FIREBASE_KEY_PREFIX);
@@ -11,8 +14,9 @@ export const getFirebaseConfig = () => {
     const name = key.slice(start);
     return [name, value];
   });
-  const remap = reword.reduce((a, [k, v]) => ({ ...a, [k]: v }), {});
-  return remap;
+  const config = reword.reduce((a, [k, v]) => ({ ...a, [k]: v }), {});
+  console.log('firebaseConfig => ', config);
+  return config;
 };
 
 export default getFirebaseConfig;
