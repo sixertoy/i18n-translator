586881c1cd5cdbb1402955c3419644ec4fdb71c0
sixertoy <sixertoy.github@gmail.com>
2020-05-12 16:15:08 +0200

    remove firebase config debug

--- src/core/utils/firebase.js
+++ src/core/utils/firebase.js
@@ -5,10 +5,8 @@ export const getFirebaseConfig = () => {
   const keys = Object.entries(ENV);
   const filtered = keys.filter(a => a[0].indexOf(FIREBASE_KEY_PREFIX) !== -1);
   if (!filtered.length) {
-    console.log('process.env => ', process.env);
     throw new Error('Unable to load firebase configuration');
   }
-  console.log('filtered => ', filtered);
   const reword = filtered.map(([key, value]) => {
     const len = FIREBASE_KEY_PREFIX.length;
     const index = key.indexOf(FIREBASE_KEY_PREFIX);
@@ -17,7 +15,6 @@ export const getFirebaseConfig = () => {
     return [name, value];
   });
   const config = reword.reduce((a, [k, v]) => ({ ...a, [k]: v }), {});
-  console.log('firebaseConfig => ', config);
   return config;
 };
 
