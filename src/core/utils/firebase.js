5c68f4b2fbc7194253c6fee2f534ac8a153ca3d6
sixertoy <sixertoy.github@gmail.com>
2020-05-12 15:24:06 +0200

    more debug for firebase/netlify

--- src/core/utils/firebase.js
+++ src/core/utils/firebase.js
@@ -4,9 +4,11 @@ const FIREBASE_KEY_PREFIX = 'FIREBASE_';
 export const getFirebaseConfig = () => {
   const keys = Object.entries(ENV);
   const filtered = keys.filter(a => a[0].indexOf(FIREBASE_KEY_PREFIX) !== -1);
-  if (filtered.length) {
+  if (!filtered.length) {
+    console.log('process.env => ', process.env);
     throw new Error('Unable to load firebase configuration');
   }
+  console.log('filtered => ', filtered);
   const reword = filtered.map(([key, value]) => {
     const len = FIREBASE_KEY_PREFIX.length;
     const index = key.indexOf(FIREBASE_KEY_PREFIX);
