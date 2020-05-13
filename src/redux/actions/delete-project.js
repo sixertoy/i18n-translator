f423458abe6f6d5f7047f4fb442dff54031663c7
sixertoy <sixertoy.github@gmail.com>
2020-05-13 21:50:40 +0200

    fix delete project action

--- src/redux/actions/delete-project.js
+++ src/redux/actions/delete-project.js
@@ -1,6 +1,6 @@
 import { EVENT_TYPES } from '../../constants';
 
-const deleteProject = project => {
+const deleteProject = ({ project }) => {
   return { project, type: EVENT_TYPES.PROJECT_DELETE };
 };
 
