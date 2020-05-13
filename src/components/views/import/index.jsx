838b5e9c8f7cd20f5e2a3ac819d7ae834fb848ee
sixertoy <sixertoy.github@gmail.com>
2020-05-13 19:10:06 +0200

    import: update styles

--- src/components/views/import/index.jsx
+++ src/components/views/import/index.jsx
@@ -3,6 +3,7 @@ import { createUseStyles, useTheme } from 'react-jss';
 import { useDispatch } from 'react-redux';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 
+import { rgba } from '../../../core/utils';
 import { createLanguageAsync } from '../../../redux/actions';
 import Steps from '../../commons/steps';
 import withLayout from '../../layout';
@@ -13,15 +14,25 @@ import Step2 from './step-select';
 import useStep from './use-step';
 
 const useStyles = createUseStyles({
-  container: {
-    composes: ['flex-rows'],
+  container: ({ theme }) => ({
+    background: theme.app.container,
+    composes: ['flex-1'],
+  }),
+  layer: ({ theme }) => ({
+    background: rgba(theme.app.layer, 0.95),
+    composes: ['flex-rows', 'items-center'],
     height: '100%',
-    margin: '48px auto 32px auto',
-    width: 800,
-  },
+    paddingBottom: 32,
+    paddingTop: 48,
+  }),
   stepper: {
-    margin: '24px auto',
+    margin: '0 auto',
     width: '80%',
+  },
+  wrapper: {
+    height: '100%',
+    maxWidth: 800,
+    width: '100%',
   },
 });
 
@@ -30,12 +41,12 @@ const steps = ['Commencer', 'Langue', 'Importer', 'Créer'];
 
 const ImportViewComponent = () => {
   const theme = useTheme();
-  const history = useHistory();
-  const dispatch = useDispatch();
   const classes = useStyles({ theme });
   const { next, project, step } = useStep();
   const [draft, setDraft] = useState(defaultState);
 
+  const history = useHistory();
+  const dispatch = useDispatch();
   const onStepChange = useCallback(
     value => {
       if (value) setDraft(value);
@@ -56,27 +67,31 @@ const ImportViewComponent = () => {
 
   return (
     <div className={classes.container} id="import-view">
-      <div className={classes.stepper}>
-        <Steps current={step - 1} steps={steps} />
+      <div className={classes.layer}>
+        <div className={classes.wrapper}>
+          <div className={classes.stepper}>
+            <Steps current={step - 1} steps={steps} />
+          </div>
+          <Switch>
+            <Route exact path="/import/:id/step/1">
+              <Step1 onSubmit={onStepChange} />
+            </Route>
+            <Route exact path="/import/:id/step/2">
+              <Step2 draft={draft} onSubmit={onStepChange} />
+            </Route>
+            <Route exact path="/import/:id/step/3">
+              <Step3 draft={draft} onSubmit={onStepChange} />
+            </Route>
+            <Route exact path="/import/:id/step/4">
+              <Step4 onSubmit={onSubmit} />
+            </Route>
+            <Route path="/import/:id/(.*)?">
+              {/* NOTE Always redirect any import paths to starting step */}
+              <Redirect to={`/import/${project}/step/1`} />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <Switch>
-        <Route exact path="/import/:id/step/1">
-          <Step1 onSubmit={onStepChange} />
-        </Route>
-        <Route exact path="/import/:id/step/2">
-          <Step2 draft={draft} onSubmit={onStepChange} />
-        </Route>
-        <Route exact path="/import/:id/step/3">
-          <Step3 draft={draft} onSubmit={onStepChange} />
-        </Route>
-        <Route exact path="/import/:id/step/4">
-          <Step4 onSubmit={onSubmit} />
-        </Route>
-        <Route path="/import/:id/(.*)?">
-          {/* NOTE Always redirect any import paths to starting step */}
-          <Redirect to={`/import/${project}/step/1`} />
-        </Route>
-      </Switch>
     </div>
   );
 };
