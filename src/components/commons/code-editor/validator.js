d5cb737117de520ddbfe3514e67222d2ffbac5e8
sixertoy <sixertoy.github@gmail.com>
2020-05-13 14:47:18 +0200

    editor: update validator

--- src/components/commons/code-editor/validator.js
+++ src/components/commons/code-editor/validator.js
@@ -1,11 +1,28 @@
+// import uniq from 'lodash.uniq';
+// const DUPLICATED_ERROR_MSG = 'Content cannot be empty';
 const EMPTY_ERROR_MSG = 'Content cannot be empty';
+
+// function checkIfNoKeysDuplicated(parsed) {
+//   const base = Object.keys(parsed);
+//   const compare = Object.keys(parsed);
+//   const uniques = uniq(compare);
+//   return uniques.length === base.length;
+// }
 
 function validate(string, allowEmpty = true) {
   try {
     const parsed = JSON.parse(string);
-    if (allowEmpty) return null;
-    const isEmpty = Object.keys(parsed).length <= 0;
-    if (isEmpty) throw new Error(EMPTY_ERROR_MSG);
+
+    if (!allowEmpty) {
+      const isEmpty = Object.keys(parsed).length <= 0;
+      if (isEmpty) throw new Error(EMPTY_ERROR_MSG);
+    }
+
+    // const noDuplicatedKeys = checkIfNoKeysDuplicated(parsed);
+    // if (!noDuplicatedKeys) {
+    //   throw new Error(DUPLICATED_ERROR_MSG);
+    // }
+
     return null;
   } catch (err) {
     // https://airbrake.io/blog/javascript-error-handling/syntaxerror-json-parse-bad-parsing
