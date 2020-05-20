d5c3576025c3691b1d952f261e8a5fbac87f8f01
sixertoy <sixertoy.github@gmail.com>
2020-05-20 10:06:31 +0200

    add useMounted hook

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -1,4 +1,5 @@
 export { default as useFile } from './use-file';
 export { default as useLogin } from './use-login';
-export { default as useTooltip } from './use-tooltip';
+export { default as useMounted } from './use-mounted';
 export { default as useScroller } from './use-scroller';
+export { default as useTooltip } from './use-tooltip';
