623ef90925aaf3a9e22df85f70cd5ab6f64e3be7
sixertoy <sixertoy.github@gmail.com>
2020-05-25 12:44:22 +0200

    add animated tick icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,9 @@
 import { ReactComponent as AiOutlineDeliveredProcedure } from './commit.svg';
 import { ReactComponent as AiOutlineTranslation } from './language.svg';
 import { ReactComponent as AiOutlineClear } from './swipe.svg';
+import { ReactComponent as AnimatedTickIcon } from './tick.svg';
 
 export { AiOutlineDeliveredProcedure as CommitIcon };
 export { AiOutlineTranslation as LanguageIcon };
 export { AiOutlineClear as SwipeIcon };
+export { AnimatedTickIcon as TickIcon };
