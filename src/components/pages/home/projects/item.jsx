e2e34c0dbbf70cd3bc24e44e057e0d78d195bec4
sixertoy <sixertoy.github@gmail.com>
2020-06-26 17:54:45 +0200

    remove favorites and drag from home items

--- src/components/pages/home/projects/item.jsx
+++ src/components/pages/home/projects/item.jsx
@@ -1,15 +1,15 @@
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
 import React from 'react';
-import {
-  AiFillPushpin as PinIconOn,
-  AiOutlinePushpin as PinIconOff,
-} from 'react-icons/ai';
-import { IoIosMenu as DragIcon } from 'react-icons/io';
+// import {
+//   AiFillPushpin as PinIconOn,
+//   AiOutlinePushpin as PinIconOff,
+// } from 'react-icons/ai';
+// import { IoIosMenu as DragIcon } from 'react-icons/io';
 import { createUseStyles } from 'react-jss';
 import { Link } from 'react-router-dom';
 
-import { useFavorite } from '../../../hooks';
+// import { useFavorite } from '../../../hooks';
 import useListStyles from '../styles';
 
 const useStyles = createUseStyles({
@@ -41,17 +41,17 @@ const GridComponent = React.memo(({ data }) => {
   const { id, name } = data;
   const queries = useStyles();
   const classes = useListStyles();
-  const { isFavorite } = useFavorite(id);
+  // const { isFavorite } = useFavorite(id);
 
   return (
     <li className={classnames(classes.item, queries.item, 'fadein')}>
-      <DragIcon className={classes.icon} />
+      {/* <DragIcon className={classes.icon} />
       {isFavorite && (
         <PinIconOn className={classnames(classes.icon, classes.favorite)} />
       )}
       {!isFavorite && (
         <PinIconOff className={classnames(classes.icon, classes.favorite)} />
-      )}
+      )} */}
       <Link
         className={classnames(classes.link, queries.link)}
         to={`/board/${id}`}>
